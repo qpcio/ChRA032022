6efd921b2f1c1be32f44304c858664d7986de532
qpcio <qpcio@onet.eu>
2022-03-16 08:38:46 +0100

    Initial commit

---
+++ src/main/java/edu/testitka/raTraining/Main.java
@@ -0,0 +1,7 @@
+package edu.testitka.raTraining;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Thanks for your help!");
+    }
+}
---
+++ src/test/java/RATests/FirstTest.java
@@ -0,0 +1,16 @@
+package RATests;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+public class FirstTest {
+    @Test
+    public void shouldRun(){
+        given()
+                .baseUri("https://api.trello.com/1/")
+                .when()
+                .get("members/me")
+                .then()
+                .statusCode(400);
+    }
+}
